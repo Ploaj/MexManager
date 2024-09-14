701666ebec4a6adce563204a0fb9fd159e51c996
Ploaj <ploaj_the_otter@yahoo.com>
2024-09-14 15:48:55 -0500

    added app icon

--- MexManager.Desktop/Program.cs
+++ MexManager.Desktop/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
 using Avalonia;
 using Avalonia.ReactiveUI;
-using GCILib.BZip2;
-using mexLib.MexScubber;
-using mexLib.Utilties;
 
 namespace MexManager.Desktop;
 
@@ -25,14 +20,4 @@ class Program
             .WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
-
-    //[STAThread]
-    //public static void Main(string[] args)
-    //{
-    //    //var dol = new MexDOL(System.IO.File.ReadAllBytes(@"C:\Users\ploaj\Desktop\Modlee\MexManager\sys\main.dol"));
-
-    //    //System.Diagnostics.Debug.WriteLine(dol.ToAddr(0x3C79D0).ToString("X8"));
-
-
-    //}
 }
